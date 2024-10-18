64822830e54dfbe0d8c2b9d531254be0c77e98c6
sebastian mendez <103006900+sebaditas@users.noreply.github.com>
2024-10-18 15:46:09 -0500

    feat: add payment-service-v3
    bugs fixed

---
+++ payment-service/src/main/java/com/dtaquito_micro_services/payments/PaymentServiceDtaquitoMicroServicesApplication.java
@@ -0,0 +1,14 @@
+package com.dtaquito_micro_services.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class PaymentServiceDtaquitoMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceDtaquitoMicroServicesApplication.class, args);
+	}
+
+}
