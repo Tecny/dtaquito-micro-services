5638387cae7cb6336ea745b74cd575e9cd4dd4ad
sebastian mendez <103006900+sebaditas@users.noreply.github.com>
2024-10-18 15:42:21 -0500

    Delete payment-service directory

--- payment-service/src/main/java/com/dtaquito_micro_services/payments/payments/domain/model/valueobjects/RoleTypes.java
+++
@@ -1,6 +0,0 @@
-package com.dtaquito_micro_services.payments.payments.domain.model.valueobjects;
-
-public enum RoleTypes {
-
-    R, P
-}
--- payment-service/src/main/java/com/dtaquito_micro_services/payments/shared/interfaces/rest/resources/MessageResource.java
+++
@@ -1,4 +0,0 @@
-package com.dtaquito_micro_services.payments.shared.interfaces.rest.resources;
-
-public record MessageResource(  ) {
-}
