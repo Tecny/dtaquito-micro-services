82927fd0de15fcd4b91fafa8b5fc1770a256b8dd
sebastian mendez <103006900+sebaditas@users.noreply.github.com>
2024-10-21 07:07:51 -0500

    Delete deposit-service directory

--- deposit-service/src/main/java/com/dtaquito_micro_services/deposit_service/deposit/domain/model/commands/CreateDepositCommand.java
+++
@@ -1,6 +0,0 @@
-package com.dtaquito_micro_services.deposit_service.deposit.domain.model.commands;
-
-import java.math.BigDecimal;
-
-public record CreateDepositCommand(Long userId, Long amount) {
-}
